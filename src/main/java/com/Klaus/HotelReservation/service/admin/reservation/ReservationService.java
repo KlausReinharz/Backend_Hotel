bc4d2792901babc5fdefd4ba53c50f472500ae28
Alex Ponce <alexponce2753@gmail.com>
2024-10-17 11:49:51 -0500

    Desarrollando del metodo para cambiar de estado en el status de reservation

--- src/main/java/com/Klaus/HotelReservation/service/admin/reservation/ReservationService.java
+++ src/main/java/com/Klaus/HotelReservation/service/admin/reservation/ReservationService.java
@@ -6,4 +6,5 @@ import com.Klaus.HotelReservation.dto.ReservationResponseDto;
 public interface ReservationService {
 
     ReservationResponseDto getAllReservations(int pageNumber);
+    boolean changeReservationStatus(Long id, String status);
 }
--- src/main/java/com/Klaus/HotelReservation/service/admin/reservation/ReservationServiceImpl.java
+++ src/main/java/com/Klaus/HotelReservation/service/admin/reservation/ReservationServiceImpl.java
@@ -2,13 +2,18 @@ package com.Klaus.HotelReservation.service.admin.reservation;
 
 import com.Klaus.HotelReservation.dto.ReservationResponseDto;
 import com.Klaus.HotelReservation.entity.Reservation;
+import com.Klaus.HotelReservation.entity.Room;
+import com.Klaus.HotelReservation.enums.ReservationStatus;
 import com.Klaus.HotelReservation.repository.ReservationRepository;
+import com.Klaus.HotelReservation.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,6 +23,8 @@ public class ReservationServiceImpl implements ReservationService{
     private final ReservationRepository reservationRepository;
 
     public static final int SEARCH_RESULT_PER_PAGE = 4;
+
+    private final RoomRepository roomRepository;
 
     public ReservationResponseDto getAllReservations(int pageNumber){
 
@@ -35,6 +42,34 @@ public class ReservationServiceImpl implements ReservationService{
 
         return reservationResponseDto;
     }
+
+    public boolean changeReservationStatus(Long id, String status){
+        Optional<Reservation>optionalReservation = reservationRepository.findById(id);
+
+        if(optionalReservation.isPresent()){
+            Reservation existingReservation = optionalReservation.get();
+
+            if(Objects.equals(status, "Approve")){
+                existingReservation.setReservationStatus(ReservationStatus.APPROVED);
+            }else{
+                existingReservation.setReservationStatus(ReservationStatus.REJECTED);
+            }
+
+            reservationRepository.save(existingReservation);
+
+            Room existingRoom = existingReservation.getRoom();
+            existingRoom.setAvailable(false);
+
+            roomRepository.save(existingRoom);
+
+
+            return true;
+        }
+
+        return false;
+    }
+
+
 
 
 
